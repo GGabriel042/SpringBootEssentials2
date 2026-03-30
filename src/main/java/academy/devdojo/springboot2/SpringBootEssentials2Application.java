5650dc2c4367c219da315bc8248602022342b432
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-03-30 18:09:25 -0300

    Spring Boot Essentials 2 - Creating Project with Spring Initializer

---
+++ src/main/java/academy/devdojo/springboot2/SpringBootEssentials2Application.java
@@ -0,0 +1,13 @@
+package academy.devdojo.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEssentials2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEssentials2Application.class, args);
+	}
+
+}
