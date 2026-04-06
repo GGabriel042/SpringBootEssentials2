eb01e4f78e0becc1ecfdce722a3d30006080be38
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-04-06 20:08:30 -0300

    Spring Boot Essentials 2 - Aula 22 Integration Test part 2

--- src/test/java/academy/devdojo/springboot2/integration/AnimeControllerIT.java
+++ src/test/java/academy/devdojo/springboot2/integration/AnimeControllerIT.java
@@ -1,5 +1,54 @@
 package academy.devdojo.springboot2.integration;
 
-@
-public class AnimeControllerIT {
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.repository.AnimeRepository;
+import academy.devdojo.springboot2.util.AnimeCreator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+class AnimeControllerIT {
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("list returns list of anime inside page object when successful")
+    void list_ReturnsListOfAnimesInsidePageObject_WhenSuccessful() {
+        Anime savedAnime = animeRepository.save(AnimeCreator.createAnimeToBeSaved());
+
+        String expectedName = savedAnime.getName();
+
+        // Usa o endpoint "/animes/all" que retorna List<Anime>
+        ResponseEntity<List<Anime>> response = testRestTemplate.exchange(
+                "/animes/all",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Anime>>() {}
+        );
+
+        List<Anime> animes = response.getBody();
+
+        Assertions.assertThat(animes).isNotNull();
+        Assertions.assertThat(animes)
+                .isNotEmpty()
+                .hasSize(1);
+
+        Assertions.assertThat(animes.get(0).getName()).isEqualTo(expectedName);
+    }
 }
