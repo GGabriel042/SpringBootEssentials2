b0b8f4832b3a94c5d576e9e5f60b690482c3ee47
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-04-09 18:35:28 -0300

    Spring Boot Essentials 2 - Aula 25 Documentação Parte 2

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.requests;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,6 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
-    @NotNull
+    @Schema(description = "This is the anime's name", example = "Dragon ball", required = true)
     private String name;
 }
